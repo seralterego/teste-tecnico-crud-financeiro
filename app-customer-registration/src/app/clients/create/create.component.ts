e5d042483ea4322fa58b3662fdb7807021fcd31f
Sérgio Leitão <sergiomiranda86@gmail.com>
2024-03-06 13:35:05 -0300

    validação de campos: cpf

--- app-customer-registration/src/app/clients/create/create.component.ts
+++ app-customer-registration/src/app/clients/create/create.component.ts
@@ -15,16 +15,16 @@ export class CreateComponent {
   formCadastro: FormGroup;
 
   constructor(
-    private clientsService: ClientsService, 
+    private clientsService: ClientsService,
     private router: Router,
     private titleService: Title,
     private fb: FormBuilder
-  ){
+  ) {
     this.titleService.setTitle("Formulário de Cadastro")
 
     this.formCadastro = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100), this.multipleNamesValidator]],
-      cpf: [0, [Validators.required]],
+      cpf: [0, [Validators.required]], //, this.cpfValidator
       birthday: ['', [Validators.required]],
       income: [0, [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
@@ -32,31 +32,10 @@ export class CreateComponent {
     })
   }
 
-  // Validador personalizado para verificar se há mais de um nome
-  private multipleNamesValidator(control: AbstractControl): { [key: string]: any } | null {
-    const value = control.value as string;
-
-    // Verificar se há pelo menos um espaço em branco
-    if (value && value.indexOf(' ') === -1) {
-      return { 'multipleNames': true };
-    }
-
-    return null;
-  }
-  /* formdata: Clients = {
-    id: 0,
-    name: '',
-    cpf: 0,
-    birthday: '',
-    income: 0,
-    email: '',
-    registered: new Date().toLocaleDateString('pt-BR'),
-  } */
-
   create() {
     if (this.formCadastro.valid) {
       const formData: Clients = this.formCadastro.value;
-      
+
       this.clientsService.creat(formData).subscribe({
         next: (data) => {
           this.router.navigate(["clients/home"]);
@@ -67,4 +46,63 @@ export class CreateComponent {
       })
     }
   }
+
+  // Validação de sobrenome
+  private multipleNamesValidator(control: AbstractControl): { [key: string]: any } | null {
+    const value = control.value as string;
+
+    // Verificar se há pelo menos um espaço em branco
+    if (value && value.indexOf(' ') === -1) {
+      return { 'multipleNames': true };
+    }
+
+    return null;
+  }
+
+  // Validação para verificar se CPF é válido
+  /* private cpfValidator(control: AbstractControl): { [key: string]: any } | null {
+    const value = control.value as string;
+
+    if (!this.isValidCPF(value)) {
+      return { 'invalidCPF': true };
+    }
+
+    return null;
+  } */
+
+  // Função para validar o CPF
+  /* private isValidCPF(cpf: string): boolean {
+    // Remove caracteres não numéricos
+    const cleanedCPF = cpf.replace(/\D/g, '');
+
+    // Verifica se possui 11 dígitos
+    if (cleanedCPF.length !== 11) {
+      return false;
+    }
+    // Verifica se todos os dígitos são iguais
+    if (/^(\d)\1+$/.test(cleanedCPF)) {
+      return false;
+    }
+    // Algoritmo de validação do CPF
+    let sum = 0;
+    let weight = 10;
+    for (let i = 0; i < 9; i++) {
+      sum += parseInt(cleanedCPF.charAt(i), 10) * weight--;
+    }
+
+    const remainder = (sum * 10) % 11;
+    if (remainder !== parseInt(cleanedCPF.charAt(9), 10)) {
+      return false;
+    }
+
+    sum = 0;
+    weight = 11;
+    for (let i = 0; i < 10; i++) {
+      sum += parseInt(cleanedCPF.charAt(i), 10) * weight--;
+    }
+
+    const secondRemainder = (sum * 10) % 11;
+
+    return secondRemainder === parseInt(cleanedCPF.charAt(10), 10);
+  } */
 }
