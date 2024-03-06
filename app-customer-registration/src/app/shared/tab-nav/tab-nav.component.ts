90f3c43c9952172233059485b106001a83bde925
Sérgio Leitão <sergiomiranda86@gmail.com>
2024-03-06 09:29:01 -0300

    rollback tentativa de ajuste posicionamento tab-nav

--- app-customer-registration/src/app/shared/tab-nav/tab-nav.component.ts
+++ app-customer-registration/src/app/shared/tab-nav/tab-nav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-tab-nav',
   templateUrl: './tab-nav.component.html',
