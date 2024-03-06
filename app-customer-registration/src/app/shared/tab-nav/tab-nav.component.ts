eccc47bc31e2a4389e7a81c3ed37589043c825be
Sérgio Leitão <sergiomiranda86@gmail.com>
2024-03-06 01:19:59 -0300

    criação do componente de navegação

---
+++ app-customer-registration/src/app/shared/tab-nav/tab-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-nav',
+  templateUrl: './tab-nav.component.html',
+  styleUrls: ['./tab-nav.component.css'],
+  standalone: true
+})
+export class TabNavComponent {
+
+}
