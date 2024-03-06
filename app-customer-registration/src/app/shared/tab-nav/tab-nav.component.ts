c8a5b78d04f8b098318d20788b1eb6a620744b94
Sérgio Leitão <sergiomiranda86@gmail.com>
2024-03-06 08:57:01 -0300

    aplicando classe active dinamicamente no tab-nav

--- app-customer-registration/src/app/shared/tab-nav/tab-nav.component.ts
+++ app-customer-registration/src/app/shared/tab-nav/tab-nav.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-tab-nav',
   templateUrl: './tab-nav.component.html',
   styleUrls: ['./tab-nav.component.css']
 })
 export class TabNavComponent {
+  items: any[] = [
+    {name: 'Cadastro', link: '/clients/create'},
+    {name: 'Lista', link: '/clients/home', active: true},
+  ];
 
+  selectedIndex = 0;
+  setActiveClass(index: number) {
+    this.selectedIndex = index;
+  }
+
+  constructor() {}
 }
